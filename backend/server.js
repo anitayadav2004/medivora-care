cf0de34725a93bd031afb7b8ba2fae91fb71e9e3
Anita Yadav <anitayadav2004@gmail.com>
2026-07-07 18:01:42 +0530

    Update backend server configuration

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ const app=express();
 const port =4000;
 
 const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:5174"
+    "https://medivora-care-frontend.onrender.com",
+    "https://medivora-care-admin.onrender.com"
 ];
 //middleware
 app.use(cors({
